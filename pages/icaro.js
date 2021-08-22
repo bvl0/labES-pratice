9c066b6a1b4c62166323bde88e009139984efcf7
bvl0 <icaro1561@outlook.com>
2021-08-22 18:46:53 -0300

    fix the prettier config

--- pages/icaro.js
+++ pages/icaro.js
@@ -1,9 +1,9 @@
-import Container from "../src/components/sandbox/sandboxContainer"
+import Container from '../src/components/sandbox/sandboxContainer'
 
 export default function Icaro() {
-  return <Container> 
-            <h1>
-                Teste
-            </h1> 
-        </Container>
+  return (
+    <Container>
+      <h1>Teste</h1>
+    </Container>
+  )
 }
--- src/components/sandbox/sandboxContainer/index.js
+++ src/components/sandbox/sandboxContainer/index.js
@@ -1,20 +1,19 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 const Container = styled.div`
-    max-width: 500px;
-    margin: 0 auto;  
-    display: flex;
-    align-Items: center;
-    justify-Content: center;
-    background-color: #fff;
-    border-radius: 4px;
-    margin-top: 50px;
-    box-shadow: 0 0 20px rgba(0,0,0,0.1);
-    
-    h1{
-        font-size: 50px;
-        color: ${({ theme }) => theme.colors.primary};
-    };
-`;
-export default Container;
+  max-width: 500px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  border-radius: 4px;
+  margin-top: 50px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
 
+  h1 {
+    font-size: 50px;
+    color: ${({ theme }) => theme.colors.primary};
+  }
+`
+export default Container
